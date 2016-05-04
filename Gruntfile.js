2f242817206130b41ee2205e020d6aa2e05e22da
Alex Krez <aleksandr.kr.26@googlemail.com>
2016-05-04 17:23:06 +0200

    structure changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,63 +1,71 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-    grunt.initConfig({
-        cssmin: {
-            target: {
-                files: [{
-                    expand: true,
-                    cwd: 'css',
-                    src: ['*.css', '!*.min.css'],
-                    dest: 'css',
-                    ext: '.min.css'
-                }]
-            }
+  grunt.initConfig({
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'dest/css',
+          src: ['*.css', '!*.min.css'],
+          dest: 'dest/css',
+          ext: '.min.css'
+        }]
+      }
+    },
+    htmlmin: { // Task
+      dist: { // Target
+        options: { // Target options
+          removeComments: true,
+          collapseWhitespace: true
         },
-        htmlmin: { // Task
-            dist: { // Target
-                options: { // Target options
-                    removeComments: true,
-                    collapseWhitespace: true
-                },
-                files: { // Dictionary of files
-                    'index.html': 'src/index.html'
-                }
-            }
-        },
-        sass: {
-            dist: {
-                options: {
-                    style: 'expanded'
-                },
-                files: {
-                    'css/style.css': 'src/scss/style.scss'
-                }
-            }
-        },
-        uglify: {
-            my_target: {
-                files: {
-                    'dest/js/output.min.js': ['js/bootstrap.js', 'js/main.js']
-                }
-            }
-        },
-        clean: ['dest/**'],
-        copy: {
-            main: {
-                expand: true,
-                src: 'src/fonts/*',
-                dest: 'dest/',
-            },
+        files: { // Dictionary of files
+          'index.html': 'src/index.html'
         }
-    });
+      }
+    },
+    sass: {
+      dist: {
+        options: {
+          style: 'expanded'
+        },
+        files: {
+          'dest/css/style.css': 'src/scss/style.scss'
+        }
+      }
+    },
+    uglify: {
+      my_target: {
+        files: {
+          'dest/js/output.min.js': ['src/js/bootstrap.js', 'src/js/main.js']
+        }
+      }
+    },
+    clean: ['dest/**'],
+    copy: {
+      main: {
+        files: [{
+          expand: true,
+          cwd: 'src',
+          src: 'fonts/*',
+          dest: 'dest/'
+        }, {
+          expand: true,
+          cwd: 'src',
+          src: 'img/*',
+          dest: 'dest/'
+        }]
+      },
+    }
+  });
 
-    grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-htmlmin');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
 
-    grunt.registerTask('default', ['clean', 'sass', 'cssmin', 'htmlmin', 'uglify', 'copy']);
+  grunt.registerTask('default', ['clean', 'sass', 'cssmin', 'htmlmin', 'uglify', 'copy']);
 
 };
