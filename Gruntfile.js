2b9c9867e65a693042f71fe88127188b20414767
Alex Krez <aleksandr.kr.26@googlemail.com>
2016-05-04 15:37:00 +0200

    htmlmin support added

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,33 @@
 module.exports = function(grunt) {
 
-  grunt.initConfig({
-    cssmin: {
-      target: {
-        files: [{
-          expand: true,
-          cwd: 'css',
-          src: ['*.css', '!*.min.css'],
-          dest: 'css',
-          ext: '.min.css'
-        }]
-      }
-    }
-  });
+    grunt.initConfig({
+        cssmin: {
+            target: {
+                files: [{
+                    expand: true,
+                    cwd: 'css',
+                    src: ['*.css', '!*.min.css'],
+                    dest: 'css',
+                    ext: '.min.css'
+                }]
+            }
+        },
+        htmlmin: {          // Task
+            dist: {         // Target
+                options: {  // Target options
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: { // Dictionary of files
+                    'index.html': 'src/index.html'
+                }
+            }
+        }
+    });
 
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
 
-  grunt.registerTask('default', ['cssmin']);
+    grunt.registerTask('default', ['cssmin', 'htmlmin']);
 
 };
